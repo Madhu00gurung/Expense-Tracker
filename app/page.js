f32ff764c94a6d3d2ce6bada9d34204c3c7e71ec
madhu gurang <madhug0916@gmail.com>
2024-07-15 00:20:42 +0530

    form done

---
+++ app/page.js
@@ -0,0 +1,16 @@
+
+"use client";
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './header.js';
+import Dashboard from "./dashboard.js";
+import './globals.css';
+export default function page() {
+  return (
+    <>
+    <Header/>
+    <Dashboard/>
+    </>
+  )
+};
+
